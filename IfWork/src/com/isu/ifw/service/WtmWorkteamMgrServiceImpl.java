97df918feef88585a0eca7dc672b3ddcad2e1214
ksj0222 <ksj0222@gmail.com>
2020-04-23 11:18:23 +0900

    근무조 종료일 변경 validation

--- IfWork/src/com/isu/ifw/service/WtmWorkteamMgrServiceImpl.java
+++ IfWork/src/com/isu/ifw/service/WtmWorkteamMgrServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Service;
 import com.isu.ifw.entity.WtmWorkteamEmp;
 import com.isu.ifw.entity.WtmWorkteamMgr;
 import com.isu.ifw.mapper.WtmFlexibleEmpMapper;
+import com.isu.ifw.mapper.WtmWorkteamEmpMapper;
 import com.isu.ifw.mapper.WtmWorkteamMgrMapper;
 import com.isu.ifw.repository.WtmWorkteamEmpRepository;
 import com.isu.ifw.repository.WtmWorkteamMgrRepository;
@@ -37,6 +38,9 @@ public class WtmWorkteamMgrServiceImpl implements WtmWorkteamMgrService{
 	@Autowired
 	WtmWorkteamMgrMapper workteamMgrMapper;
 	
+	@Autowired
+	WtmWorkteamEmpMapper workteamEmpMapper;
+
 	@Autowired
 	WtmFlexibleEmpMapper flexEmpMapper;
 	
@@ -80,16 +84,27 @@ public class WtmWorkteamMgrServiceImpl implements WtmWorkteamMgrService{
 				//List<WtmWorkteamMgr> saveList = new ArrayList();
 				if(iList != null && iList.size() > 0) {
 					for(Map<String, Object> l : iList) {
-						WtmWorkteamMgr workteam = new WtmWorkteamMgr();
-						workteam.setEnterCd(enterCd);
-						workteam.setTenantId(tenantId);
-						workteam.setUpdateId(userId);
-						workteam.setWorkteamMgrId(l.get("workteamMgrId").toString().equals("") ? null : Long.parseLong(l.get("workteamMgrId").toString()));
-						workteam.setWorkteamNm(l.get("workteamNm").toString());
-						workteam.setEymd(l.get("eymd").toString());
-						workteam.setNote(l.get("note").toString());
-						workteam.setSymd(l.get("symd").toString());
-						workteam.setFlexibleStdMgrId(Long.parseLong(l.get("flexibleStdMgrId").toString()));
+						WtmWorkteamMgr workteam = workteamMgrRepository.findByWorkteamMgrId(Long.parseLong(l.get("workteamMgrId").toString()));
+						
+						if(workteam != null) { //update는 종료일 변경, 비고만 가능
+							workteam.setNote(l.get("note").toString());
+							workteam.setEymd(l.get("eymd").toString());
+							//근무조 대상자 중 해당 근무조를사용중인 직원의 종료일이 더 크면 변경 불가
+							int emp = workteamEmpMapper.getWorkteamMgrIdAndEymd(l);
+							if(emp > 0) {
+								throw new Exception(workteam.getWorkteamNm() +" 가 할당된 직원중 종료일이 남은 직원이 종료합니다. 먼저 해당직원의 근무조 정보를  변경해주세요.");
+							}
+						} else {               //insert
+							workteam.setEnterCd(enterCd);
+							workteam.setTenantId(tenantId);
+							workteam.setUpdateId(userId);
+							workteam.setWorkteamMgrId(l.get("workteamMgrId").toString().equals("") ? null : Long.parseLong(l.get("workteamMgrId").toString()));
+							workteam.setWorkteamNm(l.get("workteamNm").toString());
+							workteam.setEymd(l.get("eymd").toString());
+							workteam.setNote(l.get("note").toString());
+							workteam.setSymd(l.get("symd").toString());
+							workteam.setFlexibleStdMgrId(Long.parseLong(l.get("flexibleStdMgrId").toString()));
+						}
 						
 						workteam = workteamMgrRepository.save(workteam);
 
