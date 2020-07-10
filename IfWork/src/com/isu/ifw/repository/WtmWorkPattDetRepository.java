c86336440b8df2869a9caada189ebd1d15ba4081
spectralfox05 <spectralfox05@gmail.com>
2020-07-10 09:28:58 +0900

    근무제 적용 관리 - wtm_work_patt_det 테이블에서 해당 근무제 패턴 건수 확인

--- IfWork/src/com/isu/ifw/repository/WtmWorkPattDetRepository.java
+++ IfWork/src/com/isu/ifw/repository/WtmWorkPattDetRepository.java
@@ -17,7 +17,9 @@ public interface WtmWorkPattDetRepository extends JpaRepository<WtmWorkPattDet, 
 	@Query(value="SELECT * FROM WTM_WORK_PATT_DET WHERE FLEXIBLE_STD_MGR_ID = :flexibleStdMgrId", nativeQuery = true)
 	public List<WtmWorkPattDet> findByFlexibleStdMgrId(@Param(value="flexibleStdMgrId")Long flexibleStdMgrId);
 	
-	//20.07.03 안흥규 삭제 시 체크
+	//20200703 안흥규 삭제 시 체크
 	public int countByTimeCdMgrId(Long timeCdMgrId);
 	public WtmWorkPattDet findByTimeCdMgrId(Long timeCdMgrId);
+	//20200709 안흥규
+	public int countByFlexibleStdMgrId(Long flexibleStdMgrId);
 }
