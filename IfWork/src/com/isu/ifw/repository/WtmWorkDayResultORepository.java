8c120755c671224a883f5ef31e27fd0ac113406f
claire <claire@10.30.30.188>
2020-08-31 13:39:43 +0900

    .

--- IfWork/src/com/isu/ifw/repository/WtmWorkDayResultORepository.java
+++ IfWork/src/com/isu/ifw/repository/WtmWorkDayResultORepository.java
@@ -11,4 +11,5 @@ import com.isu.ifw.entity.WtmWorkDayResultO;
 public interface WtmWorkDayResultORepository extends JpaRepository<WtmWorkDayResultO, Long> {
 	
 	public List<WtmWorkDayResultO> findByTenantIdAndEnterCdAndSabunAndYmd(Long tenantId, String enterCd, String sabun, String ymd);
+	public List<WtmWorkDayResultO> findByTenantIdAndEnterCdAndSabunAndYmdAndTimeTypeCdIN(Long tenantId, String enterCd, String sabun, String ymd, List<String> timeTypeCd);
 }
