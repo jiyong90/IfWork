5d65adeb70edeb3d1caae356627d5ea738408913
HughKim <raground@gmail.com>
2020-10-05 15:16:38 +0900

    근태코드 VO

---
+++ IfWork/src/com/isu/ifw/vo/WtmTaaCodeVO.java
@@ -0,0 +1,140 @@
+package com.isu.ifw.vo;
+
+public class WtmTaaCodeVO {
+
+	private Long taaCodeId;
+	private Long tenantId;
+	private String enterCd;
+	private String taaCd;
+	private String taaNm;
+	private String taaTypeCd;
+	private String holInclYn;
+	private String requestTypeCd;
+	private String workYn;
+	private Integer workApprHour;
+	private String payYn;
+	private String taaInfoCd;
+	private String note;
+	private String updateId;
+	private String updateDate;
+
+	public Long getTaaCodeId() {
+		return taaCodeId;
+	}
+
+	public void setTaaCodeId(Long taaCodeId) {
+		this.taaCodeId = taaCodeId;
+	}
+
+	public Long getTenantId() {
+		return tenantId;
+	}
+
+	public void setTenantId(Long tenantId) {
+		this.tenantId = tenantId;
+	}
+
+	public String getEnterCd() {
+		return enterCd;
+	}
+
+	public void setEnterCd(String enterCd) {
+		this.enterCd = enterCd;
+	}
+
+	public String getTaaCd() {
+		return taaCd;
+	}
+
+	public void setTaaCd(String taaCd) {
+		this.taaCd = taaCd;
+	}
+
+	public String getTaaNm() {
+		return taaNm;
+	}
+
+	public void setTaaNm(String taaNm) {
+		this.taaNm = taaNm;
+	}
+
+	public String getTaaTypeCd() {
+		return taaTypeCd;
+	}
+
+	public void setTaaTypeCd(String taaTypeCd) {
+		this.taaTypeCd = taaTypeCd;
+	}
+
+	public String getHolInclYn() {
+		return holInclYn;
+	}
+
+	public void setHolInclYn(String holInclYn) {
+		this.holInclYn = holInclYn;
+	}
+
+	public String getRequestTypeCd() {
+		return requestTypeCd;
+	}
+
+	public void setRequestTypeCd(String requestTypeCd) {
+		this.requestTypeCd = requestTypeCd;
+	}
+
+	public String getWorkYn() {
+		return workYn;
+	}
+
+	public void setWorkYn(String workYn) {
+		this.workYn = workYn;
+	}
+
+	public Integer getWorkApprHour() {
+		return workApprHour;
+	}
+
+	public void setWorkApprHour(Integer workApprHour) {
+		this.workApprHour = workApprHour;
+	}
+
+	public String getPayYn() {
+		return payYn;
+	}
+
+	public void setPayYn(String payYn) {
+		this.payYn = payYn;
+	}
+
+	public String getTaaInfoCd() {
+		return taaInfoCd;
+	}
+
+	public void setTaaInfoCd(String taaInfoCd) {
+		this.taaInfoCd = taaInfoCd;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public String getUpdateId() {
+		return updateId;
+	}
+
+	public void setUpdateId(String updateId) {
+		this.updateId = updateId;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+}
