933922bd40fa719237afcfd3e45f4462658af90a
HughKim <raground@gmail.com>
2020-11-11 14:12:27 +0900

    코드 조회 추가

--- IfWork/src/com/isu/ifw/repository/WtmCodeRepository.java
+++ IfWork/src/com/isu/ifw/repository/WtmCodeRepository.java
@@ -1,13 +1,12 @@
 package com.isu.ifw.repository;
 
-import java.util.List;
-
+import com.isu.ifw.entity.WtmCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.isu.ifw.entity.WtmCode;
+import java.util.List;
 
 @Repository
 public interface WtmCodeRepository extends JpaRepository<WtmCode, Long> {
@@ -15,4 +14,7 @@ public interface WtmCodeRepository extends JpaRepository<WtmCode, Long> {
 	//@Query(value="SELECT * FROM WTM_CODE WHERE TENANT_ID = :tenantId AND ENTER_CD = :enterCd AND date_format(now(),'%Y%m%d') BETWEEN SYMD AND EYMD AND GRP_CODE_CD = :grpCodeCd ORDER BY SEQ", nativeQuery = true)
 	@Query(value="SELECT C FROM WtmCode C WHERE C.tenantId = :tenantId AND C.enterCd = :enterCd AND :ymd BETWEEN C.symd AND C.eymd AND C.grpCodeCd = :grpCodeCd ORDER BY seq")
 	public List<WtmCode> findByTenantIdAndEnterCdAndYmdAndGrpCodeCd(@Param(value="tenantId")Long tenantId, @Param(value="enterCd")String enterCd, @Param(value="ymd")String ymd, @Param(value="grpCodeCd")String grpCodeCd);
+
+	@Query(value="SELECT C FROM WtmCode C WHERE C.tenantId = :tenantId AND C.enterCd = :enterCd AND :ymd BETWEEN C.symd AND C.eymd AND C.grpCodeCd = :grpCodeCd AND C.codeCd = :codeCd")
+	public WtmCode findByTenantIdAndEnterCdAndYmdAndGrpCodeCdAAndCodeCd(@Param(value="tenantId")Long tenantId, @Param(value="enterCd")String enterCd, @Param(value="ymd")String ymd, @Param(value="grpCodeCd")String grpCodeCd, @Param(value="codeCd")String codeCd);
 }
