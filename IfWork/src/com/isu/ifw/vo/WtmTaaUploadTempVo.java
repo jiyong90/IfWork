d14690313abf43ae96a4d5672af6cb44c6231f29
HughKim <raground@gmail.com>
2020-11-13 13:42:44 +0900

    업로드 VO

---
+++ IfWork/src/com/isu/ifw/vo/WtmTaaUploadTempVo.java
@@ -0,0 +1,122 @@
+package com.isu.ifw.vo;
+
+public class WtmTaaUploadTempVo {
+
+	private Long uploadIdx;
+	private String sabun;
+	private String empNm;
+	private String symd;
+	private String eymd;
+	private String taaCdNm;
+	private String taaDetailCdNm;
+	private String taaCd;
+	private String taaDetailCd;
+	private Long applId;
+	private String updateDate;
+	private String note;
+	private String requestTypeCd;
+
+	public Long getUploadIdx() {
+		return uploadIdx;
+	}
+
+	public void setUploadIdx(Long uploadIdx) {
+		this.uploadIdx = uploadIdx;
+	}
+
+	public String getSabun() {
+		return sabun;
+	}
+
+	public void setSabun(String sabun) {
+		this.sabun = sabun;
+	}
+
+	public String getEmpNm() {
+		return empNm;
+	}
+
+	public void setEmpNm(String empNm) {
+		this.empNm = empNm;
+	}
+
+	public String getSymd() {
+		return symd;
+	}
+
+	public void setSymd(String symd) {
+		this.symd = symd;
+	}
+
+	public String getEymd() {
+		return eymd;
+	}
+
+	public void setEymd(String eymd) {
+		this.eymd = eymd;
+	}
+
+	public String getTaaCdNm() {
+		return taaCdNm;
+	}
+
+	public void setTaaCdNm(String taaCdNm) {
+		this.taaCdNm = taaCdNm;
+	}
+
+	public String getTaaDetailCdNm() {
+		return taaDetailCdNm;
+	}
+
+	public void setTaaDetailCdNm(String taaDetailCdNm) {
+		this.taaDetailCdNm = taaDetailCdNm;
+	}
+
+	public String getTaaCd() {
+		return taaCd;
+	}
+
+	public void setTaaCd(String taaCd) {
+		this.taaCd = taaCd;
+	}
+
+	public String getTaaDetailCd() {
+		return taaDetailCd;
+	}
+
+	public void setTaaDetailCd(String taaDetailCd) {
+		this.taaDetailCd = taaDetailCd;
+	}
+
+	public Long getApplId() {
+		return applId;
+	}
+
+	public void setApplId(Long applId) {
+		this.applId = applId;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public String getRequestTypeCd() {
+		return requestTypeCd;
+	}
+
+	public void setRequestTypeCd(String requestTypeCd) {
+		this.requestTypeCd = requestTypeCd;
+	}
+}
